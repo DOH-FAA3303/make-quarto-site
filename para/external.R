5c753a6ced2b0511afa3868f741489e4212ecfd8
Frank Aragona <frank.aragona@doh.wa.gov>
2023-09-20 10:39:47 -0700

    final cut i think

--- para/external.R
+++ para/external.R
@@ -2,10 +2,8 @@ library(jsonlite)
 library(dplyr)
 library(purrr)
 
-## ---- try
 drug_json<- jsonlite::read_json("drug-enforcement-0001-of-0001.json")
 
-## ---- stop
 
 drug_df <- purrr::map_dfr(drug_json$results, unlist)
 glimpse(drug_df)
